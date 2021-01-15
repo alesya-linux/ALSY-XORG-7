5ed6a2486d062f6f9cfac3928efaf32d01f7d23c
alesya-linux <77435517+alesya-linux@users.noreply.github.com>
2021-01-15 06:49:36 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -6,9 +6,9 @@ export XORG_VIRTUAL="$2"
 # for all of the packages. 
 
 # Create the XORG_CONFIG variable to use for this parameter substitution:
-export XORG_CONFIG="--prefix=$XORG_VIRTUAL \
-                    --sysconfdir=/etc      \
-                    --localstatedir=/var   \
+export XORG_CONFIG="--prefix=$XORG_PREFIX              \
+                    --sysconfdir=$XORG_PREFIX/etc      \
+                    --localstatedir=$XORG_PREFIX/var   \
                     --disable-static"
 
 if [ "$XORG_PREFIX" == "" ]; then
@@ -26,19 +26,28 @@ mkdir -p $XORG_PREFIX/etc/profile.d/
 
 cat > $XORG_PREFIX/etc/profile.d/xorg.sh << EOF
 XORG_PREFIX="$XORG_VIRTUAL"
-XORG_CONFIG="--prefix=\$XORG_PREFIX  \
-             --sysconfdir=/etc       \
-             --localstatedir=/var    \
+XORG_CONFIG="--prefix=\$XORG_PREFIX               \
+             --sysconfdir=\$XORG_PREFIX/etc       \
+             --localstatedir=\$XORG_PREFIX/var    \
              --disable-static"
 export XORG_PREFIX XORG_CONFIG
-PATH="$XORG_VIRTUAL/bin:$PATH"
-PKG_CONFIG_PATH="$XORG_VIRTUAL/lib/pkgconfig:$PKG_CONFIG_PATH"   
-PKG_CONFIG_PATH="$XORG_VIRTUAL/share/pkgconfig:$PKG_CONFIG_PATH" 
-LIBRARY_PATH="$XORG_VIRTUAL/lib:$LIBRARY_PATH"
-C_INCLUDE_PATH="$XORG_VIRTUAL/include:$C_INCLUDE_PATH"
-CPLUS_INCLUDE_PATH="$XORG_VIRTUAL/include:$CPLUS_INCLUDE_PATH"
-ACLOCAL="aclocal -I $XORG_VIRTUAL/share/aclocal"
+PATH="$XORG_PREFIX/bin:$PATH"
+PKG_CONFIG_PATH="$XORG_PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH"   
+PKG_CONFIG_PATH="$XORG_PREFIX/share/pkgconfig:$PKG_CONFIG_PATH" 
+LIBRARY_PATH="$XORG_PREFIX/lib:$LIBRARY_PATH"
+C_INCLUDE_PATH="$XORG_PREFIX/include:$C_INCLUDE_PATH"
+CPLUS_INCLUDE_PATH="$XORG_PREFIX/include:$CPLUS_INCLUDE_PATH"
+ACLOCAL="aclocal -I $XORG_PREFIX/share/aclocal"
 export PATH PKG_CONFIG_PATH ACLOCAL LIBRARY_PATH C_INCLUDE_PATH CPLUS_INCLUDE_PATH
 EOF
 chmod 744 $XORG_PREFIX/etc/profile.d/xorg.sh
- 
+echo "$XORG_PREFIX/lib" >> /etc/ld.so.conf
+cp -a /etc/ld.so.conf $XORG_PREFIX/etc/ld.so.conf
+sed "s@/usr/X11R6@$XORG_PREFIX@g" -i /etc/man_db.conf
+cp -a /etc/man_db.conf $XORG_PREFIX/etc/man_db.conf
+# Some applications look for shared files in /usr/share/X11. Create a symbolic link to the proper location as the root user:
+mkdir -p $XORG_PREFIX/usr/share
+ln -svf $XORG_PREFIX/share/X11 $XORG_PREFIX/usr/share/X11
+# If building KDE, some cmake files look for Xorg in places other than $XORG_PREFIX. Allow cmake to find Xorg with:
+ln -svf $XORG_PREFIX $XORG_PREFIX/usr/X11R6
+ln -svf $XORG_PREFIX $XORG_PREFIX/$XORG_VIRTUAL
