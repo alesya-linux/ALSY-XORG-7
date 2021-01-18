845d61adaa163c5b25916dfcaf9a4829d945384b
sev_alesya <sev_alesya@outlook.com>
2021-01-18 06:44:14 +0000

    Mon Jan 18 06:44:14 AM GMT 2021

---
+++ .APP_CONFIG/Xorg-xterm-config.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+app="${PWD##*/}"
+version="${app##*-}"
+app="${app%-*}"
+arch="${ALSY_XORG_APP_CONFIG_ARCHIVE_TYPE}"
+sapp="$app-$version"
+
+if [ ! -f $app-$version.$arch ]; then  
+  filedwnld="http://invisible-mirror.net/archives/xterm/$app-$version.$arch"
+  wget $filedwnld -O "$app-$version".$arch --no-check-certificate
+fi
+
+app="$app-$version"
+sed 's/@alsy.app.name/'$app'/g' "Makefile.am" > "Makefile"
+
+if [ -d ../build/$app ]; then
+ rm -rfd ../build/$app
+ if [ $? -eq 0 ]; then
+   echo "clean .. [ ok ]"
+ else
+   exit 1
+ fi
+fi
+
+mkdir -p ../build/$app &&
+tar -xf "$app"."$arch" -C ../build/$app
+if [ $? -eq 0 ]; then  
+  cd ../build
+  if [ $? -eq 0 ]; then    
+    pushd $app &&    
+    if [[ -f $sapp/configure.ac && ! -x $sapp/configure ]]; then
+      cd $sapp   &&
+      libtoolize && 
+      autoreconf -fiv &&
+      cd ..
+    fi &&
+    if [ -x $sapp/configure ]; then    
+       export SOURCE_DATE_EPOCH=$(date +%s)
+      ./$sapp/configure $XORG_CONFIG && popd
+    fi 
+  fi
+fi
