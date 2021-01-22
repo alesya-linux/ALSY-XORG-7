50ef1285fa03edac3a69a41d6c7981d94c9e2732
alesya-linux <77435517+alesya-linux@users.noreply.github.com>
2021-01-22 18:01:37 +0500

    Add files via upload

---
+++ .APP_CONFIG/acl-config.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+app="${PWD##*/}"
+version="${app##*-}"
+app="${app%-*}"
+arch="tar.gz"
+sapp="acl-2.2.52"
+
+if [ ! -f $app-$version.$arch ]; then  
+  filedwnld="http://download.savannah.gnu.org/releases/$app/$app-$version.$arch"
+  wget $filedwnld -O "$app-$version".$arch --no-check-certificate
+fi
+
+app="$app-$version"
+sed 's/@alsy.app.name/'$app'/g' "Makefile.am" > "Makefile"
+
+if [ -d ../build/$app ]; then
+ rm -rfd ../build/$app
+ if [ $? -eq 0 ]; then
+   echo "clean .. [ ok ]"
+ else
+   exit 1
+ fi
+fi
+
+mkdir -p ../build/$app &&
+tar -xf "$app"."$arch" -C ../build/$app
+if [ $? -eq 0 ]; then  
+  cd ../build
+  if [ $? -eq 0 ]; then    
+    pushd $app/$sapp &&    
+    if [[ -f $sapp/configure.ac && ! -x $sapp/configure ]]; then
+      cd $sapp   &&
+      libtoolize && 
+      autoreconf -fiv &&
+      cd ..
+    fi &&
+    if [ -x configure ]; then    
+      ./configure $XORG_CONFIG && popd
+    fi 
+  fi
+fi
