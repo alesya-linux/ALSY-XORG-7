06e555724fdd7678b6590e05040beb6b1b7fb5bb
alesya-linux <77435517+alesya-linux@users.noreply.github.com>
2021-01-22 18:17:06 +0500

    Add files via upload

---
+++ .APP_CONFIG/attr-config.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+app="${PWD##*/}"
+version="${app##*-}"
+app="${app%-*}"
+arch="tar.gz"
+sapp="attr-2.4.47"
+
+if [ ! -f $app-$version.$arch ]; then  
+  filedwnld="http://download.savannah.gnu.org/releases/$app/$app-$version.$arch"
+  wget $filedwnld -O "$app-$version".$arch --no-check-certificate
+fi
+
+app="$app-$version"
+sed 's/@alsy.app.name/'$app'/g' "Makefile.am" > "Makefile"
+
+if [ -d ../build/$app ]; then
+ rm -rfd ../build/$app
+ if [ $? -eq 0 ]; then
+   echo "clean .. [ ok ]"
+ else
+   exit 1
+ fi
+fi
+
+mkdir -p ../build/$app &&
+tar -xf "$app"."$arch" -C ../build/$app
+if [ $? -eq 0 ]; then  
+  cd ../build
+  if [ $? -eq 0 ]; then    
+    pushd $app/$sapp &&    
+    if [ -x configure ]; then    
+      ./configure $XORG_CONFIG --disable-static
+    fi && popd    
+  fi
+fi
