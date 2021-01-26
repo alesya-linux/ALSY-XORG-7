c6d90984482c43f207896dd08901448cf8b7bb19
alesya-linux <77435517+alesya-linux@users.noreply.github.com>
2021-01-26 17:17:46 +0500

    Update systemd-config.sh

--- .APP_CONFIG/systemd-config.sh
+++ .APP_CONFIG/systemd-config.sh
@@ -65,7 +65,7 @@ if [ $? -eq 0 ]; then
                   -Dinstall-tests=false                       \
                   -Dldconfig=false                            \
                   -Dman=auto                                  \
-                  -Drootprefix=$XORG_PREFIX                   \
+                  -Drootprefix=$XORG_PREFIX/root              \
                   -Drootlibdir=$XORG_PREFIX/lib               \
                   -Dsplit-usr=true                            \
                   -Dsysusers=false                            \
