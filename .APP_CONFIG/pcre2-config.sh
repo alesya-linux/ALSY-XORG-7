1e57b77c495818e5ce0b1d548b614f53f6dc4ef8
alesya-linux <77435517+alesya-linux@users.noreply.github.com>
2021-01-28 03:27:01 +0000

    Update pcre2-config.sh

--- .APP_CONFIG/pcre2-config.sh
+++ .APP_CONFIG/pcre2-config.sh
@@ -34,16 +34,16 @@ if [ $? -eq 0 ]; then
       autoreconf -fiv
     fi &&
     if [ -x configure ]; then      
-      ./configure $XORG_CONFIG \
+      ./configure --prefix=$XORG_PREFIX                  \
                   --docdir=/usr/src/tools/share/doc/$app \
                   --enable-unicode                       \
                   --enable-jit                           \
                   --enable-pcre2-16                      \
                   --enable-pcre2-32                      \
-                  --enable-pcre2grep-libz && popd
-#                  --enable-pcre2grep-libbz2              \
-#                  --enable-pcre2test-libreadline         \
-#                  --disable-static && popd
+                  --enable-pcre2grep-libz                \
+                  --enable-pcre2grep-libbz2              \
+                  --enable-pcre2test-libreadline         \
+                  --disable-static && popd
     elif [ -f meson.build ]; then
       meson  --prefix=/usr/src/tools/$sapp          \
              -Dbuildtype=release                    \
