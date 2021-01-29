ef158dd5c31e1a4df4a85554a989885cfe88acdf
alesya-linux <77435517+alesya-linux@users.noreply.github.com>
2021-01-29 13:23:02 +0000

    Delete ncurses-install.sh

--- .APP_CONFIG/ncurses-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-INSTALLDIR=$1
-echo "INPUT(-lncursesw)" > ${INSTALLDIR}/usr/lib/libncurses.so
-mkdir -p ${INSTALLDIR}/usr/lib
-cp -a ${INSTALLDIR}/lib/libncursesw.so.6* ${INSTALLDIR}/usr/lib
-mv -v ${INSTALLDIR}/usr/lib/libncursesw.so.6* ${INSTALLDIR}/lib
-ln -sfv ../../lib/$(readlink ${INSTALLDIR}/usr/lib/libncursesw.so) ${INSTALLDIR}/usr/lib/libncursesw.so 
