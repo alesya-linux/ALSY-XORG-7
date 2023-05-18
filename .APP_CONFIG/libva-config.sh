2b79b906f49616498f92a2b66838463048e54d40
alesya-linux <77435517+alesya-linux@users.noreply.github.com>
2023-05-18 14:07:27 +0000

    Update libva-config.sh

--- .APP_CONFIG/libva-config.sh
+++ .APP_CONFIG/libva-config.sh
@@ -7,7 +7,7 @@ arch="tar.bz2"
 sapp="$app-$version"
 
 if [ ! -f $app-$version.$arch ]; then  
-  filedwnld="https://www.x.org/releases/individual/proto/$app-$version.$arch"
+  filedwnld="http://github.com/intel/libva/releases/download/$version/$app-$version.$arch"
   wget $filedwnld -O "$app-$version".$arch --no-check-certificate
 fi
 
@@ -24,7 +24,7 @@ if [ -d ../build/$app ]; then
 fi
 
 mkdir -p ../build/$app &&
-tar -xf "$app"."$arch" -C ../build/$app
+tar -xf "$app"."$arch" -C ../build/$app || rm -f "$app"."$arch"
 if [ $? -eq 0 ]; then  
   cd ../build
   if [ $? -eq 0 ]; then    
