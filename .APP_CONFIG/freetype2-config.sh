b39c174fe690cca748e9ce727d05f5e6abc8fa80
alesya-linux <77435517+alesya-linux@users.noreply.github.com>
2023-05-18 12:58:06 +0500

    Update freetype2-config.sh

--- .APP_CONFIG/freetype2-config.sh
+++ .APP_CONFIG/freetype2-config.sh
@@ -30,25 +30,18 @@ tar -xf "$app"."$arch" -C ../build/$app
 if [ $? -eq 0 ]; then  
   cd ../build
   if [ $? -eq 0 ]; then    
-    pushd $app &&    
-    if [[ -f $sapp/configure.ac && ! -x $sapp/configure ]]; then
-      cd $sapp   &&
-      libtoolize && 
-      autoreconf -fiv &&
-      cd ..
-    fi &&
-    if [ -x $sapp/configure ]; then 
-      python3 -m pip install -U pip &&
-      python3 -m pip install -U meson &&
-      python3 -m pip install -U docwriter &&
-      ./$sapp/configure $XORG_CONFIG        \
-                        --with-harfbuzz=yes \
-                        --with-bzip2=yes    \
-                        --with-png=yes      \
-                        --with-zlib=yes     \
-                        --enable-freetype-config && popd
+    pushd $app    
+    
+    cmake -DCMAKE_INSTALL_PREFIX=$XORG_PREFIX  \
+            -DCMAKE_BUILD_TYPE=Release \
+            -DBUILD_SHARED_LIBS=ON \
+            -DCMAKE_INSTALL_LIBDIR=lib64 \
+            $sapp
+    exitcode="$(echo $?)"
+    exit $exitcode
+    
+    popd
 # !!! first install without harfbuzz then when it is installed reinstall freetype !!!    
-# And this is the second installation of freetype using harfbuzz !!!
-    fi 
+# And this is the second installation of freetype using harfbuzz !!!    
   fi
 fi
